18f3d38b2b572d0f556795236562d8473abdb3d0
Omer Ozarslan <omer@utdallas.edu>
2018-09-27 14:57:57 -0500

    Implement sequential tests for circular buffer

--- tests/check_circbuf.c
+++ tests/check_circbuf.c
@@ -2,46 +2,89 @@
 #include <string.h>
 #include <check.h>
 
-#include "circbuf.h"
+#include "circbuf.c"
 
 #define BUFFER_SIZE (1024*1024)
 #define DATA_SIZE (50*BUFFER_SIZE)
 
 char data[DATA_SIZE];
 char buf[BUFFER_SIZE];
+const void *pbuf;
 circbuf_t *cbuf;
 int roffset;
+int roffset_next;
+int woffset;
 
-size_t compared_read(size_t len)
+int verify_read(size_t len)
 {
-    circbuf_read(cbuf, buf, len);
-    ck_assert(!memcmp(buf, data + roffset, len));
-    return len;
+    if(!memcmp(buf, data + roffset, len)) {
+        return 1;
+    }
+    int i;
+    for (i = 0; i < len; i++)
+    {
+        if (buf[i] != data[roffset + i]) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
-size_t compared_read_some(size_t len)
+int verify_input(size_t len)
+{
+    return !memcmp(pbuf, data + roffset, len);
+}
+
+size_t data_read(size_t len)
 {
     size_t consumed;
-    consumed = circbuf_read_some(cbuf, buf, len);
-    ck_assert(!memcmp(buf, data + roffset, consumed));
+    roffset = roffset_next;
+    consumed = circbuf_read(cbuf, buf, len);
+    roffset_next = roffset + consumed;
     return consumed;
 }
 
-size_t compared_input_some(size_t len)
+size_t data_read_some(size_t len)
 {
-    const void* buf;
+    size_t consumed;
+    roffset = roffset_next;
+    consumed = circbuf_read_some(cbuf, buf, len);
+    roffset_next = roffset + consumed;
+    return consumed;
+}
+
+size_t data_input_some(size_t len)
+{
     size_t input;
-    input = circbuf_input_some(cbuf, &buf, len);
-    ck_assert(!memcmp(buf, data + roffset, input));
+    pbuf = NULL;
+    roffset = roffset_next;
+    input = circbuf_input_some(cbuf, &pbuf, len);
+    ck_assert(pbuf != NULL || len == 0);
     return input;
+}
+
+size_t data_write(size_t len)
+{
+    size_t written;
+    ck_assert(woffset + len < DATA_SIZE);
+    written = circbuf_write(cbuf, data + woffset, len);
+    woffset += written;
+    return written;
+}
+
+size_t data_dispose(size_t len)
+{
+    size_t disposed;
+    disposed = circbuf_dispose_some(cbuf, len);
+    roffset_next += disposed;
+    return disposed;
 }
 
 void fill_random_data()
 {
     int i;
     srand(0);
-    for(i = 0; i < DATA_SIZE; i++)
-    {
+    for(i = 0; i < DATA_SIZE; i++) {
         data[i] = (char) rand();
     }
 }
@@ -50,14 +93,121 @@ START_TEST(test_sequential)
 {
     cbuf = circbuf_init(BUFFER_SIZE);
     roffset = 0;
+    woffset = 0;
 
-    ck_assert(circbuf_write(cbuf, data, 50) == 50);
-    ck_assert(compared_read(50) == 50);
+    ck_assert(data_write(50) == 50);
+    ck_assert(data_read(50) == 50);
+    ck_assert(verify_read(50));
+
+    ck_assert(data_write(50) == 50);
+    ck_assert(data_read_some(60) == 50);
+    ck_assert(verify_read(50));
+    ck_assert(data_read_some(60) == 0);
+
+    ck_assert(data_write(50) == 50);
+    ck_assert(data_dispose(30) == 30);
+    ck_assert(data_read_some(10) == 10);
+    ck_assert(verify_read(10));
+    ck_assert(data_read_some(20) == 10);
+    ck_assert(verify_read(10));
+
+    ck_assert(data_write(50) == 50);
+    ck_assert(data_input_some(50) == 50);
+    ck_assert(verify_input(50));
+    ck_assert(data_input_some(50) == 50);
+    ck_assert(verify_input(50));
+    ck_assert(data_input_some(60) == 50);
+    ck_assert(verify_input(50));
+    ck_assert(data_dispose(30) == 30);
+    ck_assert(data_dispose(30) == 20);
+    ck_assert(data_input_some(60) == 0);
+    ck_assert(data_read_some(60) == 0);
+
+    ck_assert(data_write(50) == 50);
+    ck_assert(data_read_some(60) == 50);
+    ck_assert(verify_read(50));
+    ck_assert(data_read_some(60) == 0);
+
+    circbuf_destroy(cbuf);
 }
 END_TEST
 
-START_TEST(test_concurrent_fuzz)
+START_TEST(test_sequential_fill)
 {
+    size_t whole_buffer_size;
+    cbuf = circbuf_init(BUFFER_SIZE);
+    roffset = 0;
+    woffset = 0;
+
+    whole_buffer_size = ((circbuf_t_*)cbuf)->size;
+
+    ck_assert(data_write(whole_buffer_size) == whole_buffer_size);
+    ck_assert(data_read(whole_buffer_size) == whole_buffer_size);
+    ck_assert(verify_read(whole_buffer_size));
+    ck_assert(data_read_some(whole_buffer_size) == 0);
+
+    circbuf_destroy(cbuf);
+}
+END_TEST
+
+START_TEST(test_sequential_read_around)
+{
+    size_t almost_until_end;
+    size_t little_more;
+    size_t some_more;
+
+    cbuf = circbuf_init(BUFFER_SIZE);
+    roffset = 0;
+    woffset = 0;
+
+    almost_until_end = ((circbuf_t_*)cbuf)->size - 5;
+    little_more = 3;
+    some_more = 7;
+
+    ck_assert(data_write(almost_until_end) == almost_until_end);
+    ck_assert(data_read(almost_until_end) == almost_until_end);
+    ck_assert(verify_read(almost_until_end));
+    ck_assert(data_read_some(almost_until_end) == 0);
+
+    ck_assert(data_write(little_more + some_more) == little_more + some_more);
+    ck_assert(data_read(little_more) == little_more);
+    ck_assert(verify_read(little_more));
+    ck_assert(data_read(some_more) == some_more);
+    ck_assert(verify_read(some_more));
+    ck_assert(data_read_some(some_more) == 0);
+
+    circbuf_destroy(cbuf);
+}
+END_TEST
+
+START_TEST(test_sequential_dispose_around)
+{
+    size_t almost_until_end;
+    size_t little_more;
+    size_t some_more;
+
+    cbuf = circbuf_init(BUFFER_SIZE);
+    roffset = 0;
+    woffset = 0;
+
+    almost_until_end = ((circbuf_t_*)cbuf)->size - 5;
+    little_more = 3;
+    some_more = 7;
+
+    ck_assert(data_write(almost_until_end) == almost_until_end);
+    ck_assert(data_dispose(almost_until_end) == almost_until_end);
+    ck_assert(data_read_some(almost_until_end) == 0);
+
+    ck_assert(data_write(little_more + some_more) == little_more + some_more);
+    ck_assert(data_dispose(little_more) == little_more);
+    ck_assert(data_dispose(some_more) == some_more);
+    ck_assert(data_read_some(some_more) == 0);
+
+    ck_assert(data_write(little_more) == little_more);
+    ck_assert(data_read(little_more) == little_more);
+    ck_assert(verify_read(little_more));
+
+    circbuf_destroy(cbuf);
 }
 END_TEST
 
@@ -70,7 +220,9 @@ Suite* circbuf_suite()
 
     tc = tcase_create("Core");
     tcase_add_test(tc, test_sequential);
-    tcase_add_test(tc, test_concurrent_fuzz);
+    tcase_add_test(tc, test_sequential_fill);
+    tcase_add_test(tc, test_sequential_read_around);
+    tcase_add_test(tc, test_sequential_dispose_around);
 
     suite_add_tcase(s, tc);
 
