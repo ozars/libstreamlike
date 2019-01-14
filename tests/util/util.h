21a9fc74ed3fae99cca7fdde2277c849a40148da
Omer Ozarslan <omer@utdallas.edu>
2019-01-14 15:23:17 -0600

    Use random bytes instead of lowercase letters
    Tests should work with random bytes, not just lowercase letters.

--- tests/util/util.h
+++ tests/util/util.h
@@ -6,7 +6,7 @@
 inline void fill_random_data(char *buf, size_t len, unsigned int seed)
 {
     for (int i = 0; i < len; i++) {
-        buf[i] = (char)('a' + (rand_r(&seed) % 26));
+        buf[i] = (char)(rand_r(&seed));
     }
 }
 
